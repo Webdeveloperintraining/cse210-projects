281bb77e1493bd1333ef52f99fa0a246197cea0f
Webdeveloperintraining <sil22006@byui.edu>
2024-06-11 18:58:16 -0700

    Prep3 activity complete

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,42 @@
 using System;
+using Microsoft.Win32.SafeHandles;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        //Console.WriteLine("Hello Prep3 World!");
+
+
+
+        //Console.Write("What is the magic number? ");
+        //string secretNumber= Console.ReadLine();
+        //int actualSecretNumber= int.Parse(secretNumber);
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+        
+        bool end = false;
+
+        while (end == false ){
+            Console.Write("What is your guess? ");
+            string guess= Console.ReadLine();
+            int intGuess= int.Parse(guess);
+        
+            if ( intGuess == number)
+            {
+                end = true;
+                Console.WriteLine("You guessed it!");
+            }
+            else if (intGuess > number)
+            {   
+                Console.WriteLine("Lower");
+            }
+            else if (intGuess < number)
+            {   
+                Console.WriteLine("Higher");
+            }
+
+        
+        }
     }
 }
